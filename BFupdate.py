b6d240c2e247eb48601e74e5ee48073121d10746
Oicebot <oicebot@qq.com>
2018-08-19 15:25:20 +0800

    增加自动更新到最新版的功能
    用bs4库抓取大脚更新记录页面，找到最新版本的编号，然后自动下载对应的插件包…

--- BFupdate.py
+++ BFupdate.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#-*- encoding:utf-8 -*-
 
 import os
 import sys
@@ -8,7 +9,10 @@ import pathlib
 import zipfile
 import urllib.request
 
-print("BigFoot 绿色插件包自动更新器  20180802 by 欧剃")
+from bs4 import BeautifulSoup
+import requests
+
+print("BigFoot 绿色插件包自动更新器  20180819 by 欧剃")
 print("--------------------------------------------")
 
 def reporthook(count, block_size, total_size):
@@ -29,13 +33,54 @@ def savefile(url, filename):
 
 TempPath = pathlib.Path("Interface")
 
-print("请输入要下载的大脚绿色插件包版本，不知道当前版本号的请访问：")
-print(" http://nga.178.com/read.php?tid=9545469 ")
+print("本程序下载的大脚绿色插件包均来自： http://nga.178.com/read.php?tid=9545469 ")
 
-version = input("例如 8.0.0.695 ： ")
+a = input("按回车开始自动检测： ")
 
-if not version:
-    quit()
+page_link = "http://bigfoot.178.com/wow/update.html"
+
+page_response = requests.get(page_link, timeout=5)
+page_content = BeautifulSoup(page_response.content, "html.parser")
+version_list = page_content.find_all(class_='tit')
+#[<span class="tit">V8.0.0.701版本说明</span>,
+# <span class="tit">V8.0.0.700版本说明</span>]
+
+current_version = str(version_list[0]).split("版本")[0].split("V")[-1]
+
+version = str(current_version)
+
+print("网络版本：", current_version)
+
+local_version = ""
+
+try:
+    with open("Interface\AddOns\BigFoot\Version.cn.lua","r",encoding='utf-8') as version_lua_file:
+        a = ""
+        a = version_lua_file.readline()
+        #local main= "8.0.0."
+        #local minor = "695"
+        big_vers = version_lua_file.readline().split('"')[1]
+        small_vers = version_lua_file.readline().split('"')[1]
+except:
+    local_version = "0.0.0.000"
+    print("本地版本： 未安装或已损坏")
+else:
+    local_version = "".join([big_vers,small_vers])
+    print("本地版本：", local_version)
+finally:
+    #version_lua_file.close()
+    print("-------------")
+
+if local_version == version:
+    a = input("目前已安装最新版本插件，是否强制更新? Y/[N]")
+    if str(a).lower() != "y":
+        print("不进行任何改动。")
+        a = input("  ------ 按回车键退出 --------")
+        quit()
+    
+a = input("按回车开始更新：")
+
+print(" ")
 
 filename = "Interface.{}.zip".format(version)
 
@@ -54,7 +99,7 @@ print(" ")
 
 if TempPath.exists():
     print("重命名旧 Interface 文件夹... ", end="")
-    TempPath.rename("Interface.old.{}".format(time.time()))
+    TempPath.rename("Interface.old.{}".format(int(time.time())))
     print("成功")
 else:
     print("无需重命名。")
@@ -72,8 +117,8 @@ if zipPath.exists():
     for name in file.namelist():
 
         file.extract(name)
-        print("    >>> 解压缩： {} / {} ".format(current,totalNum),end="\r")
         current = current + 1
+        print("    >>> 解压缩： {} / {} ".format(current,totalNum),end="\r")
         
     print(" ")
     print("完毕。")
@@ -83,4 +128,3 @@ else:
 
 a = input("  ------ 按回车键退出 --------")
         
-
